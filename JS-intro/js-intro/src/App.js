e46ae1fad9817e6370a16dea5bcccf205d51e71b
Falconknot <falkvonkrogh@gmail.com>
2023-11-07 14:13:18 +0100

    hey

--- JS-intro/js-intro/src/App.js
+++ JS-intro/js-intro/src/App.js
@@ -1,62 +1,16 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
+import { Route, Routes } from "react-router-dom";
+import Klassekart from './klassekart';
+import Profile from './profile';
 
 function App() {
-  const initialNames = [
-    'Martin', 'Mathias', 'Kevin', 'Andreas',
-    'Falk', 'Sander', 'Ylva', 'Vanessa', 'Chen',
-    'Fridtjof', 'Luz'
-  ];
-
-  const [names, setNames] = useState(initialNames);
-
-  const handleRandomize = () => {
-    setNames(shuffleArray([...names]));
-  };
 
   return (
-    <div className='container'>
-
-      <div className='learer'>
-        <div className='person'>
-          Lærer
-        </div>
-      </div>
-
-      <div className='forste_rad'>
-        {names.slice(0, 4).map((name, index) => (
-          <div key={index} className='person'>
-            {name}
-          </div>
-        ))}
-      </div>
-
-      <div className='andre_rad'>
-        {names.slice(4, 9).map((name, index) => (
-          <div key={index} className='person'>
-            {name}
-          </div>
-        ))}
-      </div>
-
-      <div className='tredje_rad'>
-        {names.slice(9).map((name, index) => (
-          <div key={index} className='person'>
-            {name}
-          </div>
-        ))}
-      </div>
-
-      <button onClick={handleRandomize}>Randomize Names</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Klassekart />} />
+      <Route path="/:profile" element={<Profile />} />
+    </Routes>
   );
 }
 
